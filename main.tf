1b57a5b17ab123153d1ad1d88dce516495e6e54b
kvs vishnu kumar <kvsvishnukumar@kvss-Mac-mini.local>
2025-04-06 23:47:47 +0530

    added extra mumbai vars

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ module "ec2_mumbai" {
   # Pass required variables to the module
   name          = "peach-red-bot"
   azs           = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
-  ami_id        = ""
-  instance_type = "t3.micro"
+  ami_id        = "ami-002f6e91abff6eb96"
+  instance_type = "t2.micro"
 
   # Specify the AWS provider for this module
   providers = {
--- providers.tf
+++ providers.tf
@@ -1,19 +1,19 @@
 provider "aws" {
-  region = "us-east-1"
-  alias  = "virginia"  
-  profile = "default"  
+  region  = "us-east-1"
+  alias   = "virginia"
+  profile = "default"
 }
 
 provider "aws" {
-  region = "ap-south-1"
-  alias  = "mumbai"  
-  profile = "default"  
+  region  = "ap-south-1"
+  alias   = "mumbai"
+  profile = "default"
 }
 
 provider "aws" {
-  region = "eu-north-1"
-  alias  = "stockholm"  
-  profile = "default"  
+  region  = "eu-north-1"
+  alias   = "stockholm"
+  profile = "default"
 }
 
 terraform {
@@ -21,12 +21,13 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
+      # configuration_aliases = [aws.virginia, aws.mumbai, aws.stockholm]
     }
   }
-  
+
   backend "s3" {
-   bucket         = "terraw-state"
-   key            = "macmini"
-   region         = "us-east-1"
+    bucket = "terraw-state"
+    key    = "macmini"
+    region = "us-east-1"
   }
 }
